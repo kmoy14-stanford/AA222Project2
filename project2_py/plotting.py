165498ddee81a462de5a15339f4c8e38fa6121cb
Kevin Moy <kmoy14@stanford.edu>
2021-04-30 13:50:09 -0700

    plotting + secret2

--- project2_py/plotting.py
+++ project2_py/plotting.py
@@ -6,8 +6,77 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
 from helpers import Simple1, Simple2
+# from project2 import optimize
+
+#%% Modify optimize here to create plots
+def optimize(f, g, c, x0, n, count, prob, optfun):
+    """
+    Args:
+        f (function): Function to be optimized
+        g (function): Gradient function for `f`
+        c (function): Function evaluating constraints
+        x0 (np.array): Initial position to start from
+        n (int): Number of evaluations allowed. Remember `f` and `c` cost 1 and `g` costs 2
+        count (function): takes no arguments are reutrns current count
+        prob (str): Name of the problem. So you can use a different strategy 
+                 for each problem. `prob` can be `simple1`,`simple2`,`simple3`,
+                 `secret1` or `secret2`
+        optfun (int): either 1 or 2 (for either penalty fn 1 or penalty fn 2)
+    Returns:
+        x_best (np.array): best selection of variables found
+    """
+
+    # TODO: Add Hooke-Jeeves in for the simple problems; try cross-entropy for larger secret problems
+    # FOr cross-entropy, denerate random samples with a Gaussian but discard those that violate constraints
+    
+    # Implement cross-entropy
+    nsamp = 100   # number of samples for each distribution
+    cov = 4*np.identity(np.size(x0)) # Initial covariance matrix
+    nelite = 20 # number of elite samples to pick for new distribution
+    c_scale = 20000
+    epsilon = 0.01
+
+    # Simple Problem-specific stuff:
+    if prob == 'simple1':
+        c_scale = 10000
+        nsamp = 200
+        nelite = 80
+        epsilon = 0
+    elif prob == 'simple2':
+        c_scale = 30000
+        nsamp = 200
+        nelite = 80
+    
+    mu = x0
+    mus = x0
+    while count() < n:
+        fs = np.zeros(nsamp)  # Function evaluated at samples
+        # Sample from distribution
+        samps = np.random.multivariate_normal(mu, cov, nsamp)
+        for j in range(nsamp):
+            consts = c(samps[j])
+            if optfun == 1:
+                # fs[j] = f(samps[j]) + c_scale*np.sum(consts > 0)
+                fs[j] = f(samps[j]) + c_scale*np.linalg.norm(np.max(consts,0))**2
+            elif optfun == 2:
+                fs[j] = f(samps[j]) + c_scale*np.sum(np.maximum(consts,0)**2)
+        # Retrieve indices with lowest function values
+        ind = np.argpartition(fs, nelite)[:nelite]
+        # recompute distribution parameters
+        cov = np.cov(samps[ind].T)
+        mu = np.mean(samps[ind],0)
+        mus = np.vstack((mus, mu))
+        # print(mu)
+    x_best = mu
+    print(mus)
+    return mus
 
 #%% Initialize vectors
+
+# Initialize instances of simple1, simple2
+p1 = Simple1()
+p2 = Simple2()
+
 x1 = np.linspace(-3,3)
 x2 = np.linspace(-3,3)
 simp1_z = np.zeros((50,50))
@@ -16,8 +85,6 @@ simp1_c2 = np.zeros((50,50))
 simp2_z = np.zeros((50,50))
 simp2_c1 = np.zeros((50,50))
 simp2_c2 = np.zeros((50,50))
-p1 = Simple1()
-p2 = Simple2()
 for i in range(50):
     for j in range(50):
         x = np.array([x1[i], x2[j]])
@@ -31,12 +98,22 @@ for i in range(50):
 
 # %% Plot Simple1
 d = np.linspace(-3,3,100)
+x0s = np.array([[0, 1], [-3,-3], [1,1]])
 x,y = np.meshgrid(d,d)
 fig, ax = plt.subplots()
 plt.imshow( ((x+y**2-1 <=0 ) & (-x-y<=0)).astype(int) , 
                 extent=(x.min(),x.max(),y.min(),y.max()),origin="lower", cmap="Greys", alpha = 0.3);
 plt.plot(-(x2**2-1), x2)
 plt.plot(x1, -x1)
+for i in range (3):
+    p1_solve = Simple1()
+    xs = optimize(p1_solve.f, p1_solve.g, p1_solve.c, x0s[i], p1_solve.n, p1_solve.count, p1_solve.prob, 2)
+    plt.plot(xs.T[0], xs.T[1], color="maroon")
+    plt.scatter(xs.T[0][-1], xs.T[1][-1], color="red")
+    # p1_solve = Simple1()
+    # xs = optimize(p1_solve.f, p1_solve.g, p1_solve.c, x0s[i], p1_solve.n, p1_solve.count, p1_solve.prob, 1)
+    # plt.plot(xs.T[0], xs.T[1], color="maroon")
+    # plt.scatter(xs.T[0][-1], xs.T[1][-1], color="red")
 plt.legend(['Constraint 1', 'Constraint 2'], bbox_to_anchor=(1.5, 0.6))
 # im = ax.imshow(simp1, interpolation='bilinear', origin='lower',
 #                cmap=cm.gray, extent=(-3, 3, -3, 3))
@@ -54,12 +131,22 @@ plt.figure(figsize=(10,10))
 # %% Plot Simple2
 
 d = np.linspace(-3,3,100)
+x0s = np.array([[-1, 1], [3,-3], [1,2]])
 x,y = np.meshgrid(d,d)
 fig, ax = plt.subplots()
 plt.imshow( (((x-1)**3 - y + 1 <=0 ) & (x + y - 2 <=0)).astype(int) , 
                 extent=(x.min(),x.max(),y.min(),y.max()),origin="lower", cmap="Greys", alpha = 0.3);
 plt.plot(x1, (x1-1)**3 + 1)
 plt.plot(x1, -(x1-2))
+for i in range (3):
+    p2_solve = Simple2()
+    xs = optimize(p2_solve.f, p2_solve.g, p2_solve.c, x0s[i], p2_solve.n, p2_solve.count, p2_solve.prob, 2)
+    plt.plot(xs.T[0], xs.T[1], color="maroon")
+    plt.scatter(xs.T[0][-1], xs.T[1][-1], color="red")    
+    # p2_solve = Simple2()
+    # xs = optimize(p2_solve.f, p2_solve.g, p2_solve.c, x0s[i], p2_solve.n, p2_solve.count, p2_solve.prob, 1)
+    # plt.plot(xs.T[0], xs.T[1], color="maroon")
+    # plt.scatter(xs.T[0][-1], xs.T[1][-1], color="red")
 plt.legend(['Constraint 1', 'Constraint 2'], bbox_to_anchor=(1.5, 0.6))
 # im = ax.imshow(simp1, interpolation='bilinear', origin='lower',
 #                cmap=cm.gray, extent=(-3, 3, -3, 3))
@@ -73,6 +160,30 @@ plt.xlabel('x1')
 plt.ylabel('x2')
 plt.grid()
 plt.figure(figsize=(10,10))
-# %% Simple1
+# %% Simple2 convergence plots
+x0s = np.array([[-1, 1], [1,-1], [1,2]])
+f_it = np.zeros((6,3))
+maxc_it = np.zeros((6,3))
+for i in range(3):
+    p2_solve = Simple2()
+    xs = optimize(p2_solve.f, p2_solve.g, p2_solve.c, x0s[i], p2_solve.n, p2_solve.count, p2_solve.prob, 1)
+    p2 = Simple2()
+    # f_it = np.zeros(6)
+    # maxc_it = np.zeros(6)
+    for j in range(np.shape(xs)[0]):
+        f_it[j,i] = p2.f(xs[j])
+        maxc_it[j,i] = np.max(p2.c(xs[j]))
+plt.plot(f_it)
+plt.legend(['x0 = [-1, 1]', 'x0 = [1, -1]', 'x0 = [1, 2]'], bbox_to_anchor=(1.35, 0.6))
+plt.xlabel('Iteration')
+plt.ylabel('Function Value')
+plt.grid()
+plt.figure(figsize=(10,10))
+plt.plot(maxc_it)
+plt.legend(['x0 = [-1, 1]', 'x0 = [1, -1]', 'x0 = [1, 2]'], bbox_to_anchor=(1.35, 0.6))
+plt.xlabel('Iteration')
+plt.ylabel('Max Constraint Value')
+plt.grid()
+plt.figure(figsize=(10,10))
 
-# Try 1 feasible point, 
+# %%
--- project2_py/project2.py
+++ project2_py/project2.py
@@ -30,85 +30,88 @@ def optimize(f, g, c, x0, n, count, prob):
     Returns:
         x_best (np.array): best selection of variables found
     """
-
-    # TODO: Add Hooke-Jeeves in for the simple problems; try cross-entropy for larger secret problems
-    # FOr cross-entropy, denerate random samples with a Gaussian but discard those that violate constraints
     
     # Implement cross-entropy
     nsamp = 100   # number of samples for each distribution
     cov = 4*np.identity(np.size(x0)) # Initial covariance matrix
     nelite = 20 # number of elite samples to pick for new distribution
-    c_scale = 20000
+    c_scale = 200
+    epsilon = 0.01
 
     # Simple Problem-specific stuff:
     if prob == 'simple1':
         c_scale = 10000
         nsamp = 200
         nelite = 80
+        epsilon = 0
     elif prob == 'simple2':
         c_scale = 30000
-        nsamp = 250
-        nelite = 100
+        nsamp = 200
+        nelite = 80
     elif prob == 'simple3':
         c_scale = 300
         nsamp = 200
-        nelite = 50
+        nelite = 80
     
     mu = x0
     if np.size(x0) > 3: # catch secret2
         nsamp = 100   # number of samples for each distribution
-        cov = 3*np.identity(np.size(x0)) # Initial covariance matrix
-        nelite = 30 # number of elite samples to pick for new distribution
-        # c_scale = 10000
-        gamma = 2
-        rho = 2
-        #TODO: Interior point method here?? Since x0 is always feasible
+        cov = 0.0001*np.identity(np.size(x0)) # Initial covariance matrix
+        nelite = 10 # number of elite samples to pick for new distribution
+        c_scale = 10000
         while count() < n:
             fs = np.zeros(nsamp)  # Function evaluated at samples
             # Sample from distribution
             samps = np.random.multivariate_normal(mu, cov, nsamp)
             for j in range(nsamp):
-                barrier = 0
+                # Employ count penalty
                 consts = c(samps[j])
-                for k in range(np.size(x0)):
-                    barrier += 1/(consts[k])
-                barrier *= -1/rho
-                fs[j] = f(samps[j]) + barrier
+                fs[j] = f(samps[j]) + c_scale * sum(consts>0)
             # Retrieve indices with lowest function values
             ind = np.argpartition(fs, nelite)[:nelite]
             # recompute distribution parameters
             cov = np.cov(samps[ind].T)
             mu = np.mean(samps[ind],0)
-            # Update interior point scaling
-            rho *= gamma
     elif np.size(x0) > 1: #all except secret1
         while count() < n:
             fs = np.zeros(nsamp)  # Function evaluated at samples
             # Sample from distribution
             samps = np.random.multivariate_normal(mu, cov, nsamp)
             for j in range(nsamp):
-                fs[j] = f(samps[j]) + c_scale*np.linalg.norm(np.max(c(samps[j]),0))**2
+                consts = c(samps[j]+epsilon)
+                fs[j] = f(samps[j]) + c_scale*np.sum(np.maximum(consts,0)**2)
             # Retrieve indices with lowest function values
             ind = np.argpartition(fs, nelite)[:nelite]
             # recompute distribution parameters
             cov = np.cov(samps[ind].T)
             mu = np.mean(samps[ind],0)
     else: # for secret1 only
-        spd = 1.0 # standard deviation of distribution
+        # Gradient hyperparameters
+        h = 0.01 # stepsize
+        x = x0
+        # Adam hyperparameters
+        gamma_v = 0.9
+        gamma_s = 0.999
+        alpha = 0.01
+        k = 0
+        v = 0
+        s = 0
+        eps = 1e-8
         while count() < n:
-            fs = np.zeros(nsamp)
-            # sample from distribution
-            samps = np.random.normal(x0, spd, nsamp)
-            for j in range(nsamp):
-                # samp = np.array(samps[j]).reshape((1,1))
-                # samp = np.array(samps[j])
-                samp = np.array([samps[j]])
-                fs[j] = f(samp) + c_scale*np.linalg.norm(np.max(c(samp),0))**2
-            # Retrieve indices with lowest function values
-            ind = np.argpartition(fs, nelite)[:nelite]
-            # recompute distribution parameters
-            spd = np.std(samps[ind])
-            mu = np.mean(samps[ind])
+            xh = x + h
+            # Approximate gradient of function + constants
+            consts = c(x)
+            constsh = c(xh)
+            f_penalty = f(x0) + c_scale*np.sum(np.maximum(consts,0)**2)
+            f_penalty_h = f(xh) + c_scale*np.sum(np.maximum(constsh,0)**2)
+            g_est = (f_penalty_h - f_penalty)/h
+            v = gamma_v*v + (1-gamma_v)*g_est
+            s = gamma_s*s + (1-gamma_s)*g_est*g_est
+            k += 1
+            v_hat = v / (1 - gamma_v**k)
+            s_hat = s / (1 - gamma_s**k)
+            x = x - alpha * v_hat / (eps + np.sqrt(s_hat))
+        mu = x
 
 
     x_best = mu
